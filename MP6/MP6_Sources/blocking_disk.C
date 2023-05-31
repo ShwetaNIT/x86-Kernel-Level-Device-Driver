bed17b20d934188ad21d63074ae842c0d8868a38
shweta_sharma_1999 <shirochou@Shwetas-MacBook-Air.local>
2023-05-30 21:50:46 -0500

    Adding Machine Problem 6 files

---
+++ MP6/MP6_Sources/blocking_disk.C
@@ -0,0 +1,66 @@
+/*
+     File        : blocking_disk.c
+
+     Author      : 
+     Modified    : 
+
+     Description : 
+
+*/
+
+/*--------------------------------------------------------------------------*/
+/* DEFINES */
+/*--------------------------------------------------------------------------*/
+
+    /* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* INCLUDES */
+/*--------------------------------------------------------------------------*/
+
+#include "assert.H"
+#include "utils.H"
+#include "console.H"
+#include "scheduler.H"
+#include "blocking_disk.H"
+
+extern Scheduler* SYSTEM_SCHEDULER;
+
+/*--------------------------------------------------------------------------*/
+/* CONSTRUCTOR */
+/*--------------------------------------------------------------------------*/
+
+BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size) 
+  : SimpleDisk(_disk_id, _size) {
+  disk_queue = new Queue();
+  Console::puts("BlockingDisk Initiated.\n");
+}
+
+/*--------------------------------------------------------------------------*/
+/* SIMPLE_DISK FUNCTIONS */
+/*--------------------------------------------------------------------------*/
+
+bool BlockingDisk::ready() {
+  return SimpleDisk::is_ready();
+}
+
+void BlockingDisk::wait_until_ready() {
+  if(!BlockingDisk::ready()){
+    Thread* curr_thread = Thread::CurrentThread();
+    disk_queue -> enqueue(curr_thread);
+    Console::puts("Disk not Available. Thread Added to Blocked Disk Queue.\n");
+    SYSTEM_SCHEDULER -> yield();
+  }
+}
+
+void BlockingDisk::read(unsigned long _block_no, unsigned char * _buf) {
+  // -- REPLACE THIS!!!
+  SimpleDisk::read(_block_no, _buf);
+
+}
+
+
+void BlockingDisk::write(unsigned long _block_no, unsigned char * _buf) {
+  // -- REPLACE THIS!!!
+  SimpleDisk::write(_block_no, _buf);
+}
---
+++ MP6/MP6_Sources/scheduler.C
@@ -0,0 +1,109 @@
+/*
+ File: scheduler.C
+ 
+ Author:
+ Date  :
+ 
+ */
+
+/*--------------------------------------------------------------------------*/
+/* DEFINES */
+/*--------------------------------------------------------------------------*/
+
+/* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* INCLUDES */
+/*--------------------------------------------------------------------------*/
+
+#include "scheduler.H"
+#include "thread.H"
+#include "console.H"
+#include "utils.H"
+#include "assert.H"
+#include "simple_keyboard.H"
+#include "blocking_disk.H"
+
+
+/*--------------------------------------------------------------------------*/
+/* DATA STRUCTURES */
+/*--------------------------------------------------------------------------*/
+
+/* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* CONSTANTS */
+/*--------------------------------------------------------------------------*/
+
+/* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* FORWARDS */
+/*--------------------------------------------------------------------------*/
+
+/* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* METHODS FOR CLASS   S c h e d u l e r  */
+/*--------------------------------------------------------------------------*/
+
+/*struct Node {
+	Thread* _thread;
+	struct Node* _next_node;
+};*/
+
+Queue* Scheduler::ready_queue;
+BlockingDisk* Scheduler::blocking_disk;
+
+Scheduler::Scheduler() {
+    //adding dummy node to the ready queue
+    Scheduler::ready_queue = new Queue();
+    Scheduler::blocking_disk = NULL;
+    Console::puts("Constructed Scheduler.\n");
+}
+
+void Scheduler::yield() {
+    //yield to a blocked thread if ready before yielding to next ready thread
+
+    if(Scheduler::blocking_disk!=NULL && Scheduler::blocking_disk->ready() && !Scheduler::blocking_disk->disk_queue->empty()) {
+        Thread* next_disk_thread = Scheduler::blocking_disk-> disk_queue -> dequeue();
+        Console::puts("Yielding To Disk Thread: "); Console::puti(next_disk_thread->ThreadId()); Console::puts(" in Blocked Queue.\n");
+        Thread::dispatch_to(next_disk_thread);
+    }else{
+
+        if(Scheduler::ready_queue->empty()){
+            Console::puts("Ready Queue Is Empty.\n");
+            assert(false);
+        }
+        //adding current thread to end of ready queue not needed, as current thread is premempted by kernal before yielding.
+
+        //dispatch to next node
+        Thread* next_ready_thread = Scheduler::ready_queue -> dequeue();
+
+        //Scheduler::ready_queue->printQueue();
+        Console::puts("Yielding To Next Thread: "); Console::puti(next_ready_thread->ThreadId()); Console::puts(" in Ready Queue.\n");
+        Thread::dispatch_to(next_ready_thread);
+    }
+}
+
+void Scheduler::resume(Thread * _thread) {
+    Scheduler::add(_thread);
+}
+
+void Scheduler::add(Thread * _thread) {
+    Scheduler::ready_queue -> enqueue(_thread);
+    //Scheduler::ready_queue->printQueue();
+    Console::puts("Added Thread To Ready Queue.\n");
+}
+
+void Scheduler::terminate(Thread * _thread) {
+    //remove the _thread from the ready queue, in case the thread sucides, before thread shutdown.
+    Scheduler::ready_queue -> removeNode(_thread);
+    //Scheduler::ready_queue->printQueue();
+    Console::puts("Thread Terminated.\n");
+    Scheduler::yield();
+}
+
+void Scheduler::add_blocking_disk(BlockingDisk* blocking_disk){
+    Scheduler::blocking_disk = blocking_disk;
+}
